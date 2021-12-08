f0861aa5a090fa02f84e7996087eda102d4ede4d
widesnow-t <89765385+widesnow-t@users.noreply.github.com>
2021-12-08 18:58:36 +0900

    変更２

--- index.php
+++ index.php
@@ -3,19 +3,19 @@ require_once __DIR__ . '/functions.php';
 $dbh = connect_db();
 
 $sql = 'SELECT * FROM branches';
-$stmt = $dbh->prepare($sql);
-$stmt->execute();
-$branches = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute();
+        $branches = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 $sql2 = 'SELECT * FROM staffs';
-$stmt = $dbh->prepare($sql2);
-$stmt->execute();
-$staffs = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $stmt = $dbh->prepare($sql2);
+        $stmt->execute();
+        $staffs = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 $sql3 = 'SELECT * FROM sales';
-$stmt = $dbh->prepare($sql3);
-$stmt->execute();
-$sales = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $stmt = $dbh->prepare($sql3);
+    $stmt->execute();
+    $sales = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 $year = '';
 $branch = '';
@@ -29,35 +29,29 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 $selected['staff'][$staff ?: ""] = "selected";
 $selected['branch'][$branch ?: ""] = "selected";
 
-
-$sql4 = <<<EOM
-SELECT
-    q.year,
-    q.month,
-    p.name staff_id,
-    o.name branch_id,
-    FORMAT(q.sale,0) total
-FROM
-    branches as o
-INNER JOIN
-    staffs as p
-ON o.id = p.branch_id
-INNER JOIN
-    sales as q
-ON p.id = q.staff_id
-WHERE
-    staff_id = :staff
-    or branch_id = :branch
-    or year = :year
-EOM;
-$stmt = $dbh->prepare($sql4);
-$stmt->bindParam(':year', $year, PDO::PARAM_INT);
-$stmt->bindParam(':staff', $staff, PDO::PARAM_INT);
-$stmt->bindParam(':branch', $branch, PDO::PARAM_INT);
-$stmt->execute();
-$bts = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-var_dump($year, $branch, $staff, $bts);
+if ($year !='') {
+        $sql4 = 'SELECT sa.year, sa.month, st.name staff_id, b.name branch_id, sa.sale total 
+        FROM branches as b INNER JOIN staffs as st ON b.id = st.branch_id 
+        INNER JOIN sales as sa ON st.id = sa.staff_id WHERE year = :year';
+        $stmt = $dbh->prepare($sql4);
+        $stmt->bindParam(':year', $year, PDO::PARAM_STR);
+        $stmt->execute(); }
+if ($branch !='') {
+        $sql4 = 'SELECT sa.year, sa.month, st.name staff_id, b.name branch_id, sa.sale total 
+        FROM branches as b INNER JOIN staffs as st ON b.id = st.branch_id 
+        INNER JOIN sales as sa ON st.id = sa.staff_id WHERE branch_id = :branch';
+        $stmt = $dbh->prepare($sql4);
+        $stmt->bindParam(':branch', $branch, PDO::PARAM_INT);
+        $stmt->execute();}
+if ($staff !='') {
+        $sql4 = 'SELECT sa.year, sa.month, st.name staff_id, b.name branch_id, sa.sale total 
+        FROM branches as b INNER JOIN staffs as st ON b.id = st.branch_id 
+        INNER JOIN sales as sa ON st.id = sa.staff_id WHERE staff_id = :staff';
+        $stmt = $dbh->prepare($sql4);
+        $stmt->bindParam(':staff', $staff, PDO::PARAM_INT);
+        $stmt->execute();
+}
+        $bts = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -109,20 +103,21 @@ var_dump($year, $branch, $staff, $bts);
                         <th class="plan-title">売上</th>
                     </tr>
                 </thead>
+                <?php foreach ($bts as $bt) : ?>
+                    <?php $sum += ($bt['total']); ?>
                 <tbody>
-                    <?php foreach ($bts as $bt) : ?>
                         <tr>
                             <th><?= h($bt['year']) ?></th>
                             <th><?= h($bt['month']) ?></th>
                             <th><?= h($bt['branch_id']) ?></th>
                             <th><?= h($bt['staff_id']) ?></th>
-                            <th><?= h($bt['total']) ?></th>
+                            <th><?= number_format( h($bt['total'])) ?></th>
                             
                         </tr>
                     <?php endforeach; ?>
                 </tbody>
             </table>
-            <p class="total">合計:<?= $sum = array_sum(array_column($bts, 'total'))?>万円</p>
+            <p class="total">合計:<?= number_format($sum) ?>万円</p>
         </div>
 
     </div>
