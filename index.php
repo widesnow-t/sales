c9c0e23428fdc43d7913933c312f94a5f4cf07f6
widesnow-t <89765385+widesnow-t@users.noreply.github.com>
2021-12-09 22:21:50 +0900

    変更５

--- index.php
+++ index.php
@@ -3,19 +3,14 @@ require_once __DIR__ . '/functions.php';
 $dbh = connect_db();
 
 $sql = 'SELECT * FROM branches';
-        $stmt = $dbh->prepare($sql);
-        $stmt->execute();
-        $branches = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$stmt = $dbh->prepare($sql);
+$stmt->execute();
+$branches = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 $sql2 = 'SELECT * FROM staffs';
-        $stmt = $dbh->prepare($sql2);
-        $stmt->execute();
-        $staffs = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-$sql3 = 'SELECT * FROM sales';
-    $stmt = $dbh->prepare($sql3);
-    $stmt->execute();
-    $sales = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$stmt = $dbh->prepare($sql2);
+$stmt->execute();
+$staffs = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 $year = '';
 $branch = '';
@@ -29,29 +24,52 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 $selected['staff'][$staff ?: ""] = "selected";
 $selected['branch'][$branch ?: ""] = "selected";
 
-if ($year !='') {
-        $sql4 = 'SELECT sa.year, sa.month, st.name staff_id, b.name branch_id, sa.sale total 
-        FROM branches as b INNER JOIN staffs as st ON b.id = st.branch_id 
-        INNER JOIN sales as sa ON st.id = sa.staff_id WHERE year = :year ORDER BY sa.year ASC, sa.month ASC, staff_id ASC, branch_id ASC';
-        $stmt = $dbh->prepare($sql4);
-        $stmt->bindParam(':year', $year, PDO::PARAM_STR);
-        $stmt->execute(); }
-if ($branch !='') {
-        $sql4 = 'SELECT sa.year, sa.month, st.name staff_id, b.name branch_id, sa.sale total 
-        FROM branches as b INNER JOIN staffs as st ON b.id = st.branch_id 
-        INNER JOIN sales as sa ON st.id = sa.staff_id WHERE branch_id = :branch ORDER BY sa.year ASC, sa.month ASC, staff_id ASC, branch_id ASC';
-        $stmt = $dbh->prepare($sql4);
-        $stmt->bindParam(':branch', $branch, PDO::PARAM_INT);
-        $stmt->execute();}
-if ($staff !='') {
-        $sql4 = 'SELECT sa.year, sa.month, st.name staff_id, b.name branch_id, sa.sale total 
-        FROM branches as b INNER JOIN staffs as st ON b.id = st.branch_id 
-        INNER JOIN sales as sa ON st.id = sa.staff_id WHERE staff_id = :staff ORDER BY sa.year ASC, sa.month ASC, staff_id ASC, branch_id ASC';
-        $stmt = $dbh->prepare($sql4);
-        $stmt->bindParam(':staff', $staff, PDO::PARAM_INT);
-        $stmt->execute();
+
+$sql4 = <<<EOM
+SELECT
+    sa.year,
+    sa.month,
+    st.name staff_id,
+    b.name branch_id,
+    sa.sale total
+FROM
+    branches as b
+INNER JOIN
+    staffs as st
+ON b.id = st.branch_id
+INNER JOIN
+    sales as sa
+ON st.id = sa.staff_id
+EOM;
+if ($year || $branch || $staff) {
+    $where = '';
+    if ($year) {
+        $where = ' sa.year = :year';
+    }
+    if ($branch) {
+        if ($where) {
+            $where .= ' AND ';
+        }
+        $where .= 'b.id = :branch ';
+    }
+    if ($staff) {
+        if ($where) {
+        $where .= ' AND ';
+        }
+        $where .= 'st.id = :staff ';
+    }
+    $where = ' WHERE ' . $where;
 }
-        $bts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$sql4 .= $where;
+$sql4 .= 'ORDER BY sa.year ASC, sa.month asc, staff_id ASC, branch_id ASC, total ASC';
+$stmt = $dbh->prepare($sql4);
+if ($year){$stmt->bindParam(':year', $year, PDO::PARAM_INT);}
+if ($staff){$stmt->bindParam(':staff', $staff, PDO::PARAM_INT);}
+if ($branch){$stmt->bindParam(':branch', $branch, PDO::PARAM_INT);}
+$stmt->execute();
+$bts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+//var_dump($year, $branch, $staff);
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -103,16 +121,15 @@ if ($staff !='') {
                         <th class="plan-title">売上</th>
                     </tr>
                 </thead>
-                <?php foreach ($bts as $bt) : ?>
-                    <?php $sum += ($bt['total']); ?>
                 <tbody>
+                    <?php foreach ($bts as $bt) : ?>
+                        <?php $sum += ($bt['total']); ?>
                         <tr>
                             <th><?= h($bt['year']) ?></th>
                             <th><?= h($bt['month']) ?></th>
                             <th><?= h($bt['branch_id']) ?></th>
                             <th><?= h($bt['staff_id']) ?></th>
-                            <th><?= number_format( h($bt['total'])) ?></th>
-                            
+                            <th><?= h($bt['total']) ?></th>
                         </tr>
                     <?php endforeach; ?>
                 </tbody>
